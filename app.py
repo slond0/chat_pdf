81500b14cd7a37e9af293aa302ec30b96cbe84ff
slond0 <137227033+slond0@users.noreply.github.com>
2026-04-09 07:31:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ st.write("Versión de Python:", platform.python_version())
 
 # Load and display image
 try:
-    image = Image.open('Chat_pdf.png')
+    image = Image.open('Chat_pdf.jpeg')
     st.image(image, width=350)
 except Exception as e:
     st.warning(f"No se pudo cargar la imagen: {e}")
